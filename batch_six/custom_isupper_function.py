23263bdce8cffbe9c76ea5355774fd21e610c9c8
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-27 22:25:23 +0800

    define_function_to_check_if_all_characters_are_uppercase

--- batch_six/custom_isupper_function.py
+++ batch_six/custom_isupper_function.py
@@ -1,4 +1,6 @@
 # define a function that checks if all characters in a string are uppercase
+def custom_isupper_function(string):
+    
     # loop through each character in the input string
         # if the character is not uppercase, return False
     # if no lowercase characters are found, return True
