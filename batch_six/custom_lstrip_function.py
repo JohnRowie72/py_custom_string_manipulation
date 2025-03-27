9c3f269caa76c7b44b20ae26b239303df5230c0c
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-27 16:13:14 +0800

    pseudocode

---
+++ batch_six/custom_lstrip_function.py
@@ -0,0 +1,5 @@
+# define a function that removes leading spaces from a string
+    # start from the beginning of the string
+    # loop until a non-space character is found
+    # return the substring starting from the non-space character
+# print the result of calling the function with the string "   hello"
