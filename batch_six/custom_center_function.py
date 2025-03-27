5e7b36a9990c45db545d1cd17a4ea97f48aa8f5c
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-27 23:05:12 +0800

    create_function_to_center_a_string

--- batch_six/custom_center_function.py
+++ batch_six/custom_center_function.py
@@ -1,4 +1,6 @@
 # define a function that centers a string by adding spaces on both sides
+def custpm_center_function(string, total_length):
+    
     # calculate the total number of spaces needed
     # determine how many spaces are needed on each side
     # create the centered string
