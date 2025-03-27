e1f823f08d0908e72ee6395bb0a9518e212b5a6a
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-27 16:31:50 +0800

    loop_through_string_to_find_first_nono_space_character

--- batch_six/custom_lstrip_function.py
+++ batch_six/custom_lstrip_function.py
@@ -5,5 +5,10 @@ def custom_lstrip(string):
     i = 0
 
     # loop until a non-space character is found
+    while i < len(string) and string[i] == " ":
+        i += 1
+
     # return the substring starting from the non-space character
+    return string[i:]
+    
 # print the result of calling the function with the string "   hello"
