e88a93cbe1e327817f865f0bcdd08642a727812c
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-27 22:27:47 +0800

    loop_through_characters_and_check_for_lowercase_letters_then_return_true_if_no_lowercase_letters_were_found

--- batch_six/custom_isupper_function.py
+++ batch_six/custom_isupper_function.py
@@ -1,8 +1,15 @@
 # define a function that checks if all characters in a string are uppercase
 def custom_isupper_function(string):
-    
+
     # loop through each character in the input string
+    for character in string:
+
         # if the character is not uppercase, return False
+        if 'a' <= character <= 'z':
+            return False
+
     # if no lowercase characters are found, return True
+    return True
+    
 # print the result of calling the function with the string "HELLO WORLD"
 # print the result of calling the function with the string "Hello World"
