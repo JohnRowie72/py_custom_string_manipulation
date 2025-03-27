299f6da42eb39ba0b2deed2e651a2fd5082e11f4
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-27 22:48:45 +0800

    pseudocode

---
+++ batch_six/custom_ljust_function.py
@@ -0,0 +1,5 @@
+# define a function that left-justifies a string by adding it with spaces at the end
+    # calculate the number of spaces needed
+    # if spaces are needed, add them to the end of the string
+    # if the string is already long enough, return it as is
+# print the result of calling the function with the string "hello" and width 10 
