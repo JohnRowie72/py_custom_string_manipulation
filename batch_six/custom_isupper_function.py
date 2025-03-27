f44ef6e4386bb1b527713348cc7cdcafeb5a310f
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-27 22:24:21 +0800

    pseudocode

---
+++ batch_six/custom_isupper_function.py
@@ -0,0 +1,6 @@
+# define a function that checks if all characters in a string are uppercase
+    # loop through each character in the input string
+        # if the character is not uppercase, return False
+    # if no lowercase characters are found, return True
+# print the result of calling the function with the string "HELLO WORLD"
+# print the result of calling the function with the string "Hello World"
