18b4e54b224e0aa714af89057dfa09918b978fa9
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-27 22:36:18 +0800

    pseudocode

---
+++ batch_six/custom_endswith_function.py
@@ -0,0 +1,6 @@
+# define a function that checks if a string ends with a specific suffix
+    # get the length of the suffix
+    # remove the last 'suffix_length' characters from the input string
+    # compare the removed part with the suffix and return the result
+# print the result of calling the function with the string "hello world" and suffix "world"
+# print the result of calling the function with the string "hello world" and suffix "hello"
