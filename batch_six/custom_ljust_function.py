83b77a25d7807ad5c3d8d23127b4e069ab40b9bd
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-27 22:56:05 +0800

    calculate_and_add_necessary_spaces

--- batch_six/custom_ljust_function.py
+++ batch_six/custom_ljust_function.py
@@ -1,7 +1,14 @@
 # define a function that left-justifies a string by adding it with spaces at the end
 def custom_ljust_function(string, total_length):
-    
+
     # calculate the number of spaces needed
+    spaces_needed = total_length - len(string)
+
     # if spaces are needed, add them to the end of the string
+    if spaces_needed > 0:
+        return string + " " * spaces_needed
+
     # if the string is already long enough, return it as is
+    return string
+    
 # print the result of calling the function with the string "hello" and width 10 
