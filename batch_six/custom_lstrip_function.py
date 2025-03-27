10c78b89e2d47fe44838165893200c0e3bb2792a
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-27 16:27:47 +0800

    initialize_function_to_remove_leading_spaces

--- batch_six/custom_lstrip_function.py
+++ batch_six/custom_lstrip_function.py
@@ -1,5 +1,9 @@
 # define a function that removes leading spaces from a string
+def custom_lstrip(string):
+
     # start from the beginning of the string
+    i = 0
+
     # loop until a non-space character is found
     # return the substring starting from the non-space character
 # print the result of calling the function with the string "   hello"
