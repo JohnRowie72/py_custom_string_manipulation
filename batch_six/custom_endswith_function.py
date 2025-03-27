01f1362bcc80973ff4b3e13f461762437b53ab8a
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-27 22:38:07 +0800

    create_function_to_check_string_suffix

--- batch_six/custom_endswith_function.py
+++ batch_six/custom_endswith_function.py
@@ -1,4 +1,6 @@
 # define a function that checks if a string ends with a specific suffix
+def custom_endswith_function(string, suffix):
+    
     # get the length of the suffix
     # remove the last 'suffix_length' characters from the input string
     # compare the removed part with the suffix and return the result
