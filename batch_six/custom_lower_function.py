3b45cee2d898137776ebb830cfb7c3b2aac62acd
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-27 22:11:51 +0800

    loop_through_characters_to_change_uppercase_to_lowercase_then_return_final_lowercase_string

--- batch_six/custom_lower_function.py
+++ batch_six/custom_lower_function.py
@@ -3,9 +3,18 @@ def custom_lowercase_function(string):
 
     # initialize an empty string to store the result
     result_of_string = ""
-    
+
     # loop through each character in the input string
+    for character in string:
+
         # if the character is uppercase, convert it to lowercase
-        # otherwise, leave it unchanged
+        if 'A' <= character <= 'Z':
+            result_of_string += chr(ord(character) + 32)
+        else:
+            # otherwise, leave it unchanged
+            result_of_string += character
+
     # return the result string
+    return result_of_string
+    
 # print the result of calling the function with the string "HeLLo, WoRLd!"
