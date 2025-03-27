50cbb0d2ef78fd9db9206907d2accb65a633335f
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-27 21:43:11 +0800

    create_function_to_remove_a_specified_prefix

--- batch_six/custom_removeprefix_function.py
+++ batch_six/custom_removeprefix_function.py
@@ -1,4 +1,6 @@
 # define a function that removes a specific prefix from a string:
+def custom_remove_prefix(string, prefix):
+    
     # get the length of the prefix
     # check if the input string starts with the prefix
         # if it does, return the substring after the prefix
