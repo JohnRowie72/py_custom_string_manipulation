730488317c8dbd2138a1e791d5c19b7146aa75f3
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-27 22:40:17 +0800

    remove_and_compare_ending_substring

--- batch_six/custom_endswith_function.py
+++ batch_six/custom_endswith_function.py
@@ -1,8 +1,14 @@
 # define a function that checks if a string ends with a specific suffix
 def custom_endswith_function(string, suffix):
-    
+
     # get the length of the suffix
+    suffix_length = len(suffix)
+
     # remove the last 'suffix_length' characters from the input string
+    remove_last_suffix = string[-suffix_length:]
+
     # compare the removed part with the suffix and return the result
+    return remove_last_suffix == suffix
+    
 # print the result of calling the function with the string "hello world" and suffix "world"
 # print the result of calling the function with the string "hello world" and suffix "hello"
