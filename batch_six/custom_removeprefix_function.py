bddba4eb9b1f4e7d5011afcfc1dbfa406bfd777a
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-27 21:46:34 +0800

    check_if_string_starts_with_the_prefix_then_remove_prefix_and_return_result

--- batch_six/custom_removeprefix_function.py
+++ batch_six/custom_removeprefix_function.py
@@ -2,7 +2,15 @@
 def custom_remove_prefix(string, prefix):
     
     # get the length of the prefix
+    prefix_length = len(prefix)
+
     # check if the input string starts with the prefix
+    if string[:prefix_length] == prefix:
+
         # if it does, return the substring after the prefix
+        return string[prefix_length:]
+
     # otherwise, return the input string as is
+    return string
+    
 # print the result of calling the function with the string "helloworld" and prefix "hello"
