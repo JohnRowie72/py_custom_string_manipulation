b90010a37326ec3535eab2abae97a34f8c46894e
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-27 22:09:22 +0800

    define_function_to_convert_string_to_lowercase_and_initialize_an_empty_string

--- batch_six/custom_lower_function.py
+++ batch_six/custom_lower_function.py
@@ -1,5 +1,9 @@
 # define a function that converts all characters of a string to lowercase
+def custom_lowercase_function(string):
+
     # initialize an empty string to store the result
+    result_of_string = ""
+    
     # loop through each character in the input string
         # if the character is uppercase, convert it to lowercase
         # otherwise, leave it unchanged
