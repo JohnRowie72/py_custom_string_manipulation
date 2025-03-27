0b80793977b8b13b30d6ecb0ba8f5ea29e99367d
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-27 21:38:57 +0800

    pseudocode

---
+++ batch_six/custom_removeprefix_function.py
@@ -0,0 +1,6 @@
+# define a function that removes a specific prefix from a string:
+    # get the length of the prefix
+    # check if the input string starts with the prefix
+        # if it does, return the substring after the prefix
+    # otherwise, return the input string as is
+# print the result of calling the function with the string "helloworld" and prefix "hello"
