f69ed6d64e3385521d13a2e1fb8f9002400c4273
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-27 16:35:01 +0800

    variable_name_change

--- batch_six/custom_lstrip_function.py
+++ batch_six/custom_lstrip_function.py
@@ -2,14 +2,14 @@
 def custom_lstrip(string):
 
     # start from the beginning of the string
-    i = 0
+    index = 0
 
     # loop until a non-space character is found
-    while i < len(string) and string[i] == " ":
-        i += 1
+    while index < len(string) and string[index] == " ":
+        index += 1
 
     # return the substring starting from the non-space character
-    return string[i:]
+    return string[index:]
     
 # print the result of calling the function with the string "   hello"
 print(custom_lstrip("   hello"))  # "hello"
