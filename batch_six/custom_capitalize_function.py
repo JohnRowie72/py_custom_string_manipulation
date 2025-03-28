cfca65282c7e0f9cb38931fb9e978543af2c28b7
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-28 20:04:32 +0800

    pseudocode

---
+++ batch_six/custom_capitalize_function.py
@@ -0,0 +1,6 @@
+# define a function that capitalizes the first character of a string and converts the rest to lowercase
+    # if the string is empty, return it unchanged
+    # convert the first letter to uppercase if it is lowercase
+    # convert the rest of the letters to lowercase
+    # return the final capitalized string
+# print the result of calling the function with the string "hELLO, WORLD!"
