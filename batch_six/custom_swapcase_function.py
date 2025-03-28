e639dc6a7a96825d37fbb28c0d1ca5eb90e2fbea
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-28 19:57:03 +0800

    loop_through_characters_and_switch_their_case_and_return_final_swapped_case_string

--- batch_six/custom_swapcase_function.py
+++ batch_six/custom_swapcase_function.py
@@ -3,10 +3,23 @@ def custom_swapcase_function(string):
 
     # initialize an empty string to store the result
     result_of_string = ""
-    
+
     # loop through each character in the input string
+    for character in string:
+
         # if the character is uppercase, convert it to lowercase
+        if 'A' <= character <= 'Z':
+            result_of_string += chr(ord(character) + 32)
+
         # if the character is lowercase, convert it to uppercase
+        elif 'a' <= character <= 'z':
+            result_of_string += chr(ord(character) - 32)
+
+        else:    
             # otherwise, leave it unchanged
+            result_of_string += character
+
     # return the result string
+    return result_of_string
+    
 # print the result of calling the function with the string "Hello, World!"
