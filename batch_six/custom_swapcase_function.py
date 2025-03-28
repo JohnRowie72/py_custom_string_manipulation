47c9c371035bb5d36a822576e986b02f13f2b75f
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-28 19:53:41 +0800

    define_function_to_swap_case_of_characters_and_initialize_an_empty_string

--- batch_six/custom_swapcase_function.py
+++ batch_six/custom_swapcase_function.py
@@ -1,5 +1,9 @@
 # define a function that swaps the case of each character in a string
+def custom_swapcase_function(string):
+
     # initialize an empty string to store the result
+    result_of_string = ""
+    
     # loop through each character in the input string
         # if the character is uppercase, convert it to lowercase
         # if the character is lowercase, convert it to uppercase
