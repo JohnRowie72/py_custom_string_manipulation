4b7f06cf81fd8bc5a900bc52d0eec0adccc60d9c
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-04-01 11:41:59 +0800

    define_fucntion_and_check_if_the_string_starts_with_prefix

--- batch_seven/custom_startswith_function.py
+++ batch_seven/custom_startswith_function.py
@@ -1,5 +1,9 @@
 # define a function that checks if a string starts with a specified prefix
+def custom_startswith_function(string, prefix):
+
     # check if the string starts with the specified prefix
+    return string[:len(prefix)] == prefix
+    
 # get user input for string
 # get user input for prefix
 # call the function with user input and print the result
