0e94638d81175dd9d97c3a3a31c6ba77b5132e6c
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-04-01 12:07:14 +0800

    pseudocode

---
+++ batch_seven/custom_zfill_funtion.py
@@ -0,0 +1,5 @@
+# define a function that pads a string with zeros to a specified width
+    # add zeros at the beginning if the string is shorter than the width
+# get user input for string
+# get user input for width
+# call the function with user input and print the result
