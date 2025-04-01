7458fa4f92b17d9fffee3ca81cd4fd35fa41aa59
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-04-01 11:22:18 +0800

    fix_pseudocode

--- batch_seven/custom_islower_function.py
+++ batch_seven/custom_islower_function.py
@@ -2,4 +2,5 @@
     # loop through each character in the string
         # if any character is uppercase, return False
     # if the loop completes, return True
+# get user input for string
 # call the function with user input and print the result
