a24550b6a18a32bc4a052648f260cf87db298276
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-04-01 10:50:01 +0800

    define_a_function_to_remove_a_suffix_from_string

--- batch_seven/custom_remove_suffix_function.py
+++ batch_seven/custom_remove_suffix_function.py
@@ -1,6 +1,14 @@
 # define a function that removes a specified suffix from a string
+def custom_removesuffix_function(string, suffix):
+
     # check if the string ends with the specified suffix
+    if string.endswith(suffix):
+
         # return string without suffix
+        return string[:-len(suffix)]
+
     # otherwise, return the original string
+    return string
+    
 # get user input for string and suffix
 # call the function with user input and print the result  
