12539199c19a9fbaad6c385f5557e9ade5993833
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-04-01 10:40:36 +0800

    define_function_to_remove_trailing_spaces_from_a_string_and_initialize_index_to_track_the_last_non_space_character

--- batch_seven/custom_rstrip.py
+++ batch_seven/custom_rstrip.py
@@ -1,5 +1,9 @@
 # define a function that removes trailing whitespace from a string
+def custom_rstrip_function(string):
+
     # initialize index to the last character's position
+    index = len(string) - 1
+    
     # loop while index is within bounds and character at index is a space
     # return substring without trailing spaces
 # get user input
