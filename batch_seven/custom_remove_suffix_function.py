b6b62e302d541d8855500e71d7903efb4f3fa57a
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-04-01 10:53:22 +0800

    ask_for_input_and_print_the_result

--- batch_seven/custom_remove_suffix_function.py
+++ batch_seven/custom_remove_suffix_function.py
@@ -9,6 +9,10 @@ def custom_removesuffix_function(string, suffix):
 
     # otherwise, return the original string
     return string
-    
+
 # get user input for string and suffix
-# call the function with user input and print the result  
+string_name_input = input("Enter a string: ")
+suffix = input("Enter a suffix to remove: ")
+
+# call the function with user input and print the result
+print(custom_removesuffix_function(string_name_input, suffix))
