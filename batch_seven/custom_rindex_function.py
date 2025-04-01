5816658dbbb9abb14d87652683b873ddbb7a6ec5
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-04-01 13:20:23 +0800

    pseudocode

---
+++ batch_seven/custom_rindex_function.py
@@ -0,0 +1,6 @@
+# define a function that finds the last occurrence of a substring in a string
+    # loop through the string backwards while checking for substring matches
+    # if substring is not found, raise an error
+# get user input for string
+# get user input for substring
+# try to call the function with user input and print the result
