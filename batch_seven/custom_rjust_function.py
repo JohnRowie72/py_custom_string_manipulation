14cf0b778e50aa0967236bba2b3b01596788ef4b
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-04-01 12:00:06 +0800

    calculate_and_apply_padding_if_the_string_is_shorter_than_width

--- batch_seven/custom_rjust_function.py
+++ batch_seven/custom_rjust_function.py
@@ -1,7 +1,9 @@
 # define a function that right-justifies a string to a specified width
-def custom_rjust_function(string, width):
+def custom_rjust_function(string, width, fill_character=' '):
 
     # add spaces at the beginning if the string is shorter than the width
+    return fill_character * (width - len(string)) + string if len(string) < width else string
+    
 # get user input for string
 # get user input for width
 # call the function with user input and print the result
