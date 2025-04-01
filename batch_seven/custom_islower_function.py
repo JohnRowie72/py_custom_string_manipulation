cff5ad6f1427c542020094bcb036767ae9bb28c6
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-04-01 11:21:44 +0800

    pseudocode

---
+++ batch_seven/custom_islower_function.py
@@ -0,0 +1,5 @@
+# define a function that checks if a string is lowercase
+    # loop through each character in the string
+        # if any character is uppercase, return False
+    # if the loop completes, return True
+# call the function with user input and print the result
