fb60e2c479f17332c7b2b7f209ac53b45008d75c
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-04-01 11:45:13 +0800

    ask_for_input_and_print_the_result

--- batch_seven/custom_startswith_function.py
+++ batch_seven/custom_startswith_function.py
@@ -3,7 +3,12 @@ def custom_startswith_function(string, prefix):
 
     # check if the string starts with the specified prefix
     return string[:len(prefix)] == prefix
-    
+
 # get user input for string
+string_name_input = input("Enter a string: ")
+
 # get user input for prefix
+prefix = input("Enter a prefix to check: ")
+
 # call the function with user input and print the result
+print(custom_startswith_function(string_name_input, prefix))
