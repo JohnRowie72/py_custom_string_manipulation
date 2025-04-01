30ad38b6e732a22f96bc45def8143dc1a4989dfe
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-04-01 11:56:32 +0800

    implement_custom_function_for_right_aligning_a_string

--- batch_seven/custom_rjust_function.py
+++ batch_seven/custom_rjust_function.py
@@ -1,4 +1,6 @@
 # define a function that right-justifies a string to a specified width
+def custom_rjust_function(string, width):
+
     # add spaces at the beginning if the string is shorter than the width
 # get user input for string
 # get user input for width
