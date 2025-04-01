841e9a7d2c16a46ca1d4a7c8eb221ee505662818
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-04-01 12:56:42 +0800

    pseudocode

---
+++ batch_seven/custom_index_function.py
@@ -0,0 +1,6 @@
+# define a function that returns the position at the first occurrence of the specified value.
+    # loop through the string while checking for substring matches
+    # if substring is not found, raise an error
+# get user input for string
+# get user input for substring
+# try to call the function with user input and print the result
