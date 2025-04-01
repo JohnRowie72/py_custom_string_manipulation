75dcc3d1a7c3287feaab3f4d0f39340b7cd12b31
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-04-01 13:02:14 +0800

    loop_through_string_and_return_the_index_of_the_first_occurrence

--- batch_seven/custom_index_function.py
+++ batch_seven/custom_index_function.py
@@ -1,7 +1,12 @@
 # define a function that returns the position at the first occurrence of the specified value.
 def custom_index_function(string, substring):
-    
+
     # loop through the string while checking for substring matches
+    for index in range(len(string) - len(substring) + 1):
+        # if substring is found, return the index
+        if string[index:index + len(substring)] == substring:
+            return index
+
     # if substring is not found, raise an error
 # get user input for string
 # get user input for substring
