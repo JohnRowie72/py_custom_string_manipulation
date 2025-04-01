ea1942280231120b7ce69694cf9c5d6185f6c9f9
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-04-01 12:14:30 +0800

    ask_input_and_print_the_result

--- batch_seven/custom_zfill_funtion.py
+++ batch_seven/custom_zfill_funtion.py
@@ -3,7 +3,12 @@ def custom_zfill_function(string, width):
 
     # add zeros at the beginning if the string is shorter than the width
     return '0' * (width - len(string)) + string if len(string) < width else string
-    
+
 # get user input for string
+string_name_input = input("Enter a string: ")
+
 # get user input for width
+width = int(input("Enter the width: "))
+
 # call the function with user input and print the result
+print(custom_zfill_function(string_name_input, width))
