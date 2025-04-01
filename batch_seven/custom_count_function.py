22f770f9557565d560f71595a5c7ab541b8589fb
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-04-01 12:40:20 +0800

    implement_custom_function_to_count_substring_occurrences

--- batch_seven/custom_count_function.py
+++ batch_seven/custom_count_function.py
@@ -1,4 +1,6 @@
 # define a function that counts the occurrences of a substring in a string
+def custom_count_function(string, substring):
+    
     # initialize count to 0
     # loop through the string while checking for substring matches
     # return the count of occurrences
