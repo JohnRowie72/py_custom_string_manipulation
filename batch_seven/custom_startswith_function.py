c11c6d5de4b0db8168a476cc518188994b1f5c57
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-04-01 11:35:41 +0800

    pseudocode

---
+++ batch_seven/custom_startswith_function.py
@@ -0,0 +1,5 @@
+# define a function that checks if a string starts with a specified prefix
+    # check if the string starts with the specified prefix
+# get user input for string
+# get user input for prefix
+# call the function with user input and print the result
