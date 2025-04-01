f03d4c958c12a132a51d4a477dc65e35df29386d
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-04-01 10:47:28 +0800

    pseudocode

---
+++ batch_seven/custom_remove_suffix_function.py
@@ -0,0 +1,6 @@
+# define a function that removes a specified suffix from a string
+    # check if the string ends with the specified suffix
+        # return string without suffix
+    # otherwise, return the original string
+# get user input for string and suffix
+# call the function with user input and print the result  
