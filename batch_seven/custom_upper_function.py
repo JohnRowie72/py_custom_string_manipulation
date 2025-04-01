3b7c535b5797222726e67ab40a84782ead736e02
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-04-01 11:06:37 +0800

    define_function_to_convert_string_to_uppercase_and_initialize_empty_string_to_store_result

--- batch_seven/custom_upper_function.py
+++ batch_seven/custom_upper_function.py
@@ -1,5 +1,9 @@
 # define a function that converts a string to uppercase
+def custom_upper_function(string):
+
     # initialize an empty string to store the result
+    result = ""
+    
     # loop through each character in the input string
         # if the character is lowercase, convert it to uppercase and append to result
     # return the uppercase string
