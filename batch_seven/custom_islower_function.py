0492bfeead6213bf5dba65e66ff915b945f63667
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-04-01 11:28:19 +0800

    check_if_any_character_is_uppercase_and_return_true_if_no_uppercase_found

--- batch_seven/custom_islower_function.py
+++ batch_seven/custom_islower_function.py
@@ -3,8 +3,13 @@ def custom_islower_function(string):
 
     # loop through each character in the string
     for character in string:
-        
+
         # if any character is uppercase, return False
+        if 'A' <= character <= 'Z':
+            return False
+
     # if the loop completes, return True
+    return True
+
 # get user input for string
 # call the function with user input and print the result
