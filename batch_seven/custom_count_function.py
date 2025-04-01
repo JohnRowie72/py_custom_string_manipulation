f92d9dbf84239b6a8e5969202d56769d1057dacf
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-04-01 12:18:06 +0800

    pseudocode

---
+++ batch_seven/custom_count_function.py
@@ -0,0 +1,7 @@
+# define a function that counts the occurrences of a substring in a string
+    # initialize count to 0
+    # loop through the string while checking for substring matches
+    # return the count of occurrences
+# get user input for string
+# get user input for substring
+# call the function with user input and print the result
