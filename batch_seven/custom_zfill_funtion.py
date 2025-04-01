52793adb4ef96800e26492c384b80cabac809c9d
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-04-01 12:12:51 +0800

    calculate_and_apply_zero_padding_if_the_string_is_shorter_than_width

--- batch_seven/custom_zfill_funtion.py
+++ batch_seven/custom_zfill_funtion.py
@@ -1,7 +1,9 @@
 # define a function that pads a string with zeros to a specified width
 def custom_zfill_function(string, width):
-    
+
     # add zeros at the beginning if the string is shorter than the width
+    return '0' * (width - len(string)) + string if len(string) < width else string
+    
 # get user input for string
 # get user input for width
 # call the function with user input and print the result
