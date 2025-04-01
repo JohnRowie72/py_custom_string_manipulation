e019e7ff3e907134720b3d6063e65ac472bb7ddb
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-04-01 13:09:11 +0800

    ask_for_input_then_handle_errors_and_print_the_result

--- batch_seven/custom_index_function.py
+++ batch_seven/custom_index_function.py
@@ -8,6 +8,16 @@ def custom_index_function(string, substring):
             return index
 
     # if substring is not found, raise an error
+    raise ValueError(f"'{substring}' is not in the string")
+
 # get user input for string
+string_name_input = input("Enter a string: ")
+
 # get user input for substring
+substring = input("Enter a substring to find its index: ")
+
 # try to call the function with user input and print the result
+try:
+    print(custom_index_function(string_name_input, substring))
+except ValueError as Error:
+    print(Error)
