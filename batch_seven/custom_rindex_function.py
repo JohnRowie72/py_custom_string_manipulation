a0f166387e5b4bcae4d486e5d541aa8150577475
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-04-01 13:32:54 +0800

    ask_for_input_then_handle_errors_and_print_the_result

--- batch_seven/custom_rindex_function.py
+++ batch_seven/custom_rindex_function.py
@@ -7,8 +7,18 @@ def custom_rindex_function(string, substring):
         # if substring is found, return the index
         if string[index:index + len(substring)] == substring:
             return index
-            
+
     # if substring is not found, raise an error
+    raise ValueError(f"'{substring}' is not in the string")
+    
 # get user input for string
+string_name_input = input("Enter a string: ")
+
 # get user input for substring
+substring = input("Enter a substring to find its last index: ")
+
 # try to call the function with user input and print the result
+try:
+    print(custom_rindex_function(string_name_input, substring))
+except ValueError as Error:
+    print(Error)
