c27325aebab228ee76bbb86d5ad6f76bea91b484
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-04-01 12:01:03 +0800

    get_input_and_print_the_result

--- batch_seven/custom_rjust_function.py
+++ batch_seven/custom_rjust_function.py
@@ -3,7 +3,12 @@ def custom_rjust_function(string, width, fill_character=' '):
 
     # add spaces at the beginning if the string is shorter than the width
     return fill_character * (width - len(string)) + string if len(string) < width else string
-    
+
 # get user input for string
+string_name_input = input("Enter a string: ")
+
 # get user input for width
+width = int(input("Enter the width: "))
+
 # call the function with user input and print the result
+print(custom_rjust_function(string_name_input, width))
